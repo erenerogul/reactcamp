e9040286f49f9612f7c6cdc532d418fc3aea721d
Eren Eroğul <eren.eroul1903@gmail.com>
2023-09-26 10:16:43 +0300

    26 eylül eklentiler 1

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Categories from './Categories'
+import ProductList from '../pages/ProductList'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Categories/>
+            <ProductList/>
+        </div>
+    )
+}
